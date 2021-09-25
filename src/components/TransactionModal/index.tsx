4c05539a3dde88347ff6e2775d7581ef001780ca
mardsonferreira <mardsonferreira25@gmail.com>
2021-09-25 15:01:17 -0300

    Adding styles to modal

--- src/components/TransactionModal/index.tsx
+++ src/components/TransactionModal/index.tsx
@@ -1,4 +1,7 @@
 import Modal from "react-modal";
+import closeImg from "../../assets/close.svg";
+
+import { Container } from "./styles";
 
 interface transactionModalProps {
   isOpen: boolean;
@@ -10,8 +13,29 @@ export function TransactionModal({
   onRequestClose,
 }: transactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Create Transaction</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Close" />
+      </button>
+
+      <Container>
+        <h2>Create Transaction</h2>
+
+        <input placeholder="Title" />
+        <input type="number" placeholder="Value" />
+        <input placeholder="Category" />
+
+        <button type="submit">Create</button>
+      </Container>
     </Modal>
   );
 }
