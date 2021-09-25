0ad047e73c0ffbbd892dbc7aa12c768abd9769ad
mardsonferreira <mardsonferreira25@gmail.com>
2021-09-24 22:15:37 -0300

    Creating transactions table component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Value</th>
+                        <th>Category</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td className="deposit">R$12.000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">-R$1.000,00</td>
+                        <td>Moradia</td>
+                        <td>20/02/2021</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </Container>
+    )
+}
